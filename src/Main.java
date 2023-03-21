d2e7b88effbf98c3994513922abd326ab192f65d
Berfin Yılmaz <berfin.adiyan12@gmail.com>
2023-03-21 18:24:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Insan ilkInsan = new Insan();
+        ilkInsan.konsolaYazdir();
+        ilkInsan.konsolaYazdir();
+    }
+}
